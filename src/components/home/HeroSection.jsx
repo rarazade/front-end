0417f4672e12b4417a23fde642f4b9ca3aca177b
rarazade <isabellaadr13@gmail.com>
2025-08-13 11:30:18 +0700

    repush git

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -28,8 +28,7 @@ export default function HeroSection() {
         backgroundImage: `url(${images[currentIndex]})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
-      }}
-    >
+      }}>
       {/* Overlay */}
       <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/20 to-transparent z-10" />
 
